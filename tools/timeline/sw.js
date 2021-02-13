808aa97c4a6222328c29840267b9c94e2f1111c0
Minoru Akagi <akaginch@gmail.com>
2021-02-13 14:19:24 +0900

    use service worker to cache files

---
+++ tools/timeline/sw.js
@@ -0,0 +1,39 @@
+const assets = [
+	'./',
+
+	'./timeline.png',
+
+	'./timeline.css',
+
+	'./jquery.js',
+	'./localStorageList.js',
+	'./timeline.js'
+];
+
+const cacheName = 'timeline202102';
+
+self.addEventListener('install', (event) => {
+
+	event.waitUntil(
+		caches.open(cacheName).then((cache) => {
+			return cache.addAll(assets);
+		})
+	);
+});
+
+self.addEventListener('activate', (event) => {
+
+});
+
+self.addEventListener('fetch', (event) => {
+
+	event.respondWith(caches.match(event.request).then((cacheResponse) => {
+
+		return cacheResponse || fetch(event.request).then((response) => {
+			return caches.open(cacheName).then((cache) => {
+				cache.put(event.request, response.clone());
+				return response;
+			});
+		});
+	}));
+});
