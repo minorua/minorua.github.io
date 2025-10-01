9dcf57dbdf289e54b616623b6464041b12495862
Minoru Akagi <akaginch@gmail.com>
2025-10-01 13:44:01 +0900

    [Copilot] randomchoice: add service-worker.js

---
+++ tools/copilot-generated/randomchoice/service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'randomchoice-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './main.js',
+  './style.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
